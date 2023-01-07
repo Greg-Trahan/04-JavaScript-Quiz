484093fdd61f0e583d004230354e163529978a52
Greg Trahan <trahan.greg@gmail.com>
2023-01-06 18:11:42 -0600

    added functionality to high scores

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 // Generate global variables
 const generateBtn = document.querySelector("#generate");
 const buttonDiv = document.querySelector(".start-button");
+const highScoreBtn = document.querySelector("#high-score")
 let numberField = document.querySelector("#intro");
 let questionField = document.querySelector("#instructions");
 let optionsField = document.querySelector("#options");
 let optionsList = document.querySelector("#options-list")
+let viewScores = document.querySelector(".view")
+let scoreList = document.querySelector("#score-list")
 let questionNumber = 1;
 let count = 0
 let correct = 0
 let done = false
+var leaderBoard = []
+
+
+
 
 // Generate timer vairable and set the time available
 const timeElement = document.querySelector(".timer");
-let secondsLeft = 60
+let secondsLeft = 5
 timeElement.textContent = secondsLeft + " seconds";
 
 // An array of objects detailing the questions, options, and answers
@@ -28,12 +35,13 @@ const questionArray = [
   answer: "Mew" },
 
   {
-    question: "Which pokemon can learn the move fly?", 
+    question: "Which pokemon can learn the move Fly?", 
     options: ["Pikachu", "Bulbasaur", "Mew", "Vulpix"], 
     answer: "Mew" }
 ]
 
-// Add event listener to button to start the quiz
+//When on the main page you can either start the quiz, or view the high score page
+highScoreBtn.addEventListener("click", showHighScore);
 generateBtn.addEventListener("click", startQuiz);
 
 // Start the quiz and the timer
@@ -64,13 +72,13 @@ function timer(){
 function questionGeneration(questionNumber){
   if (count+1 <= questionArray.length){
     //Clear previous elements
-
+    scoreList.innerHTML = ""
     optionsList.innerHTML = ""
 
     numberField.textContent= `Question #${count+1}`;
     questionField.textContent = questionArray[count].question
     
-    //Create an unordered list of buttons with the attribute response
+    //Create an unordered list of buttons with the attribute 'response'
     questionArray[count].options.forEach(function (option, idx){
       let li = document.createElement("li");
       let button = document.createElement("button");
@@ -81,7 +89,6 @@ function questionGeneration(questionNumber){
       li.appendChild(button);
       optionsList.appendChild(li);
     })
-
   } else {
     finish()
   }
@@ -115,13 +122,16 @@ function wrongAnswer(){
   questionGeneration()
 }
 
+//Display score and let user type initials
 function finish(){
   done = true
-  buttonDiv.innerHTML = ""
   let form = document.createElement("form");
   let type = document.createElement("input");
   const submitBtn = document.createElement("input");
+  buttonDiv.innerHTML = ""
+  optionsList.innerHTML = ""
 
+  //Create submit button and text field for user to iput initials
   submitBtn.setAttribute("type", "submit");
   submitBtn.setAttribute("value", "Submit");
   // form.setAttribute("style", "padding: 5px")
@@ -131,34 +141,88 @@ function finish(){
   form.appendChild(type);
   optionsField.appendChild(form);
   form.appendChild(submitBtn);
+  
 
-  optionsList.innerHTML = ""
   numberField.textContent= `You got ${correct}/3 correct. Congraguations!`;
   questionField.textContent = "Please enter your initials"
+
   submitBtn.addEventListener("click", submitScore)
-  
+
 }
 
-function submitScore(){
-  let score = []
+//Pull scores from local storage, add new score and restore new scores
+function submitScore(event){
+  event.preventDefault();
+  let score = {};
   let initialField = document.querySelector("#initials").value;
-  score = [{name: initialField, score: correct}]
+  score = {name: initialField, score: correct};
   
-  var leaderBoard = JSON.parse(localStorage.getItem("highScores"));
+  leaderBoard = JSON.parse(localStorage.getItem("highScores"));
+  if (leaderBoard === null){
+    leaderBoard = []
+  };
+
+  leaderBoard.push(score);
+  localStorage.setItem("highScores", JSON.stringify(leaderBoard));
+  
+  showHighScore();
+}
+
+
+//Pull up an ordered list of the high scores
+function showHighScore(leaderBoard){
+  leaderBoard = JSON.parse(localStorage.getItem("highScores"));
   if (leaderBoard === null){
     leaderBoard = []
   }
 
-  leaderBoard.push(score)
-  localStorage.setItem("highScores", JSON.stringify(leaderBoard));
+  console.log(leaderBoard)
+
+  //Clear previous content
+  numberField.textContent = "High Scores"
+  questionField.innerHTML = ""
+  viewScores.innerHTML = ""
+  optionsField.innerHTML = ""
+
+  //Recreate Start Button --- This is a mess. I know this is a mess. It's because earlier I was trying to do everything with the HTML elements I already had instead of just making new ones for all of my new things I wanted to appear.
+  buttonDiv.innerHTML = ""
+  generateBtn.setAttribute("id", "start");
+  generateBtn.setAttribute("class", "btn");
+  generateBtn.textContent = "Play again?";
+  buttonDiv.appendChild(generateBtn);
+
+  console.log(leaderBoard.name)
+
+  //Create Start Button
+  const resetBtn = document.createElement("button");
+  resetBtn.setAttribute("id", "reset");
+  resetBtn.setAttribute("class", "btn");
+  resetBtn.textContent = "Reset High Scores";
+  buttonDiv.appendChild(resetBtn);
+
+  //Display high scores
+  leaderBoard.forEach(function(score, i){
+    let liTag = document.createElement("li");
+    let pTag = document.createElement("p");
+    scoreList.appendChild(liTag)
+    liTag.appendChild(pTag)
+    pTag.textContent = `${leaderBoard[i]}, ${i}`
+  })
+
+
   
-  showHighScore(leaderBoard)
+  //Reset button
+  resetBtn.addEventListener("click", function(){
+    console.log(leaderBoard)
+    leaderBoard = []
+    localStorage.setItem("highScores", JSON.stringify(leaderBoard));
+  })
 }
 
 
-//Function to list the high scores
-function showHighScore(leaderBoard){
+//Color on ul will cascade down
 
-}
-
-
+  // leaderBoard.sort(function(object1, object2){
+  //   (a.score > b.score) ? 1 : -1
+  // })
+  // list.sort((a, b) => (a.color > b.color) ? 1 : -1)
